af3829a609d93dd48b68ee46404f1e6e639d9581
Kelly Masuda <kmasuda@mymail.mines.edu>
2015-04-06 15:30:56 -0600

    Added user_type to User model

--- src/unit12/app/models/user.rb
+++ src/unit12/app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable
          # :recoverable, :trackable, :validatable
+  enum user_type: [ :admin, :user ]
 end
