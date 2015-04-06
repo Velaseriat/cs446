2210bedda0d26ca41474485819dad8bddf8ba899
Kelly Masuda <kmasuda@mymail.mines.edu>
2015-04-06 16:26:59 -0600

    Changed some user/admin database stuff

--- src/unit12/app/models/user.rb
+++ src/unit12/app/models/user.rb
@@ -2,6 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable
+  before_save :set_default_type
          # :recoverable, :trackable, :validatable
   enum user_type: [ :admin, :user ]
+
+  def set_default_type
+  	if !self.user_type
+  		self.user_type = 1
+  		self.save
+  	end
+  end
 end
