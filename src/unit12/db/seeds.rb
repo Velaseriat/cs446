1f192ffa126c948d6329153307bf2a02781e3cfb
Thomas Powell <shallowtempo@gmail.com>
2015-04-06 20:53:11 -0600

    fixed image seeding

--- src/unit12/db/seeds.rb
+++ src/unit12/db/seeds.rb
@@ -6,13 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Pet.create(name: "Rover", age: "6 years", description: "Brown doge", pet_type: :dog, pet_status: :available, image_file: "1.jpg")
-Pet.create(name: "Not Rover", age: "600 years", description: "Brown cate", pet_type: :cat, pet_status: :available, image_file: "2.jpg")
-Pet.create(name: "Bunny", age: "62 seconds", description: "Brown bunnie", pet_type: :bunny, pet_status: :available, image_file: "3.jpg")
-Pet.create(name: "Roger", age: "6 months", description: "Brown doge", pet_type: :dog, pet_status: :available, image_file: "4.jpg")
-Pet.create(name: "Rover", age: "6 years", description: "Red doge", pet_type: :cat, pet_status: :available, image_file: "5.jpg")
-Pet.create!(name: "Hey", age: "6 eons", description: "Purple doge", pet_type: :bunny, pet_status: :available, image_file: "6.jpg")
-Pet.create! :name => "Hsey", :age => "6 eons", :description => "Purple doge", :pet_type => :bunny, :pet_status => :available, :image_file => 'home-cat.jpg'
+Pet.create(name: "Rover", age: "6 years", description: "Brown doge", pet_type: :dog, pet_status: :available, image_file: File.open(File.join(Rails.root, "public/pets/images/1.jpg")))
+Pet.create(name: "Not Rover", age: "600 years", description: "Brown cate", pet_type: :cat, pet_status: :available, image_file: File.open(File.join(Rails.root, "public/pets/images/2.jpg")))
+Pet.create(name: "Bunny", age: "62 seconds", description: "Brown bunnie", pet_type: :bunny, pet_status: :available, image_file: File.open(File.join(Rails.root, "public/pets/images/3.jpg")))
+Pet.create(name: "Roger", age: "6 months", description: "Brown doge", pet_type: :dog, pet_status: :available, image_file: File.open(File.join(Rails.root, "public/pets/images/4.jpg")))
+Pet.create(name: "Rover", age: "6 years", description: "Red doge", pet_type: :cat, pet_status: :available, image_file: File.open(File.join(Rails.root, "public/pets/images/5.jpg")))
+Pet.create!(name: "Hey", age: "6 eons", description: "Purple doge", pet_type: :bunny, pet_status: :available, image_file: File.open(File.join(Rails.root, "public/pets/images/6.jpg")))
 
 Shelter.create(name: "Rowen Knights Shelter");
 
