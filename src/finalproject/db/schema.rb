bbb8c393a20bdf76e98d615d4f5a163b102a8b19
aaflack18 <kmasuda@mymail.mines.edu>
2015-04-21 20:03:10 -0600

    Finally done setting up

--- src/finalproject/db/schema.rb
+++ src/finalproject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422010406) do
+ActiveRecord::Schema.define(version: 20150422015741) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150422010406) do
     t.integer  "age"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_type"
   end
 
 end
